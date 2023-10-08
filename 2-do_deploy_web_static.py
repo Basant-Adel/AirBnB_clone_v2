486639a01c18e90a3f2a682e8e6501e5f2d22e9c
Basant-Adel <basantadel99@gmail.com>
2023-10-08 15:44:11 -0700

    aa

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,39 +6,44 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.220.124', '54.160.81.217']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
-
-
-def do_deploy(archive_path):
-    """ Do -> Deploy """
+@task
+def do_pack():
+    """ do pack """
     try:
-        if not (path.exists(archive_path)):
-            return False
+        f_current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = f'web_static_{f_current_time}.tgz web_static'
+        local("mkdir -p versions")
+        local(f"tar -cvzf versions/{file_name}")
+        return "versions/"
+    except Exception as e:
+        return None
 
-        put(archive_path, '/tmp/')
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
 
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+@task
+def do_deploy(archive_path):
+    """ do deploy """
+    env.hosts = ['100.25.220.124', '54.160.81.217']
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        for host in env.hosts:
+            env.host_string = host
+            filename = archive_path.split('/')[-1]
+            filename = filename.split('.')[0]
+            put(archive_path, '/tmp/')
+            run(f'mkdir -p /data/web_static/releases/{filename}/')
+            run(f'tar -xzf /tmp/{filename}.tgz -C \
+                /data/web_static/releases/{filename}/')
+            run(f'rm /tmp/{filename}.tgz')
+            run(f'mv /data/web_static/releases/{filename}/web_static/* \
+                /data/web_static/releases/{filename}/')
+            run(
+                f'rm -rf /data/web_static/releases/{filename}/web_static')
+            run(f'rm -rf /data/web_static/current')
+            run(f'ln -s /data/web_static/releases/{filename}/ \
+                /data/web_static/current')
+            print('New version deployed!')
 
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-        run('sudo mv /data/web_static/releases/web_static_{}\
-                        /web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'.format(timestamp))
-
-        run('sudo rm -rf /data/web_static/current')
-
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
+        return True
     except Exception as e:
         return False
-
-    return True

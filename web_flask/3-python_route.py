329794890dcdeee8b3d7a36d915467bdedaa3d87
Basant-Adel <37676351+Basant-Adel@users.noreply.github.com>
2023-10-22 17:38:44 +0300

    3-python_route :D

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application """
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ C -> followed by Text """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """ IS COOL """
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    """ Listening on 0.0.0.0, port 5000 """
+    app.run(host='0.0.0.0', port=5000)

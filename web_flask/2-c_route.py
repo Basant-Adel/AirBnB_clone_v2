360528a7e4ec4064fb18ab434b671ee957a713c3
Basant-Adel <37676351+Basant-Adel@users.noreply.github.com>
2023-10-22 17:32:09 +0300

    C_route :D

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ C -> followed by Text """
+    change_text = text.replace("_", " ")
+    return "C {}".format(change_text)
+
+
+if __name__ == "__main__":
+  """ Listening on 0.0.0.0, port 5000 """
+    app.run(host="0.0.0.0")
